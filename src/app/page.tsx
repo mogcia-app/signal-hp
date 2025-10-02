c5f7ab40297d440176cf45103e219295b9bfd7ff
marina ishida <mmmrn0@icloud.com>
2025-10-03 00:00:09 +0900

    Fix team plan: change '全SNS対応' to '複数SNS対応'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1111,7 +1111,7 @@ export default function Home() {
                 <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
               </svg>
             </div>
-            <span className="text-slate-700">全SNS対応</span>
+            <span className="text-slate-700">複数SNS対応</span>
           </div>
           
           <div className="flex items-center">
