8a7a6382d528439fe5abc9aead8272b9f7aab61d
marina ishida <mmmrn0@icloud.com>
2026-09-04 13:30:09 +0900

    Remove old sgnalapp redirect

--- next.config.ts
+++ next.config.ts
@@ -5,32 +5,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.sgnalapp.com',
-          },
-        ],
-        destination: 'https://www.signalapp.jp/:path*',
-        permanent: true,
-      },
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'sgnalapp.com',
-          },
-        ],
-        destination: 'https://www.signalapp.jp/:path*',
-        permanent: true,
-      },
-    ];
-  },
   // もし画像の最適化を使うなら↓
   // images: {
   //   domains: ['example.com'],
